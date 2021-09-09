4ffcdab0938b915d48e636daaeced2bd46f9b3ee
wanganmao <525987762@qq.com>
2021-09-09 17:58:20 +0800

    feat: init

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <header class="header" style="background-color: chocolate;">
+    asdfasdf
+  </header>
+  <main class="main" style="background-color: cornflowerblue;">
+    <nav class="nav" style="background-color: lightblue;">
+      asdf
+    </nav>
+    <section class="section" style="background-color: darkcyan;">
+      <header class="location" style="background-color: lightgreen;">
+        asdfasdf
+      </header>
+      <router-view class="view"/>
+      <footer class="footer" style="background-color: lightsteelblue;">
+        1111
+      </footer>
+    </section>
+  </main>
+</template>
+
+<style>
+@import url(./styles/index.css);
+</style>
